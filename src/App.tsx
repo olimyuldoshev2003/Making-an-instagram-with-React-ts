739cfeaea886e6d0ce45b9ae2594c1e67397fdb1
Olim Yuldoshev <oyuldoshev39@gmail.com>
2023-08-19 13:10:59 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from './Layout/Layout';
+import NotFound from './pages/NotFound/NotFound';
+import Home from './pages/Home/Home';
+import Reels from './pages/Reels/Reels';
+import Explore from './pages/Explore/Explore';
+import Messages from './pages/Messages/Messages';
+import Profile from './pages/Profile/Profile';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+
+
+const App = () => {
+  const router = createBrowserRouter([
+  //Login
+  {
+    path:"/",
+    element: <SignIn />,
+    },
+  //SignUp
+  {
+    path: "signup",
+    element: <SignUp />,
+    },
+  //Home
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "explore",
+        element: <Explore />,
+      },
+      {
+        path: "reels",
+        element: <Reels />,
+      },
+      {
+        path: "messages",
+        element: <Messages />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+  // The `state` arg is correctly typed as `RootState` already
+
+  // omit rendering logic
+
+  return (
+    // <div>
+
+    // </div>
+
+    <RouterProvider router={router} />
+  );
+}
+
+export default App
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+
+  // The `state` arg is correctly typed as `RootState` already
+  
+  const Home = () => {
+   
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home
